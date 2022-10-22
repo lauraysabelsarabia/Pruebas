34c1255c4883b3e9f5d1251524465d73a0c52916
laurasarabia <lauraysabelsarabia@gmail.com>
2022-10-22 17:46:30 +0200

    Initial commit

---
+++ app/src/main/java/com/lysm/holamundolaura/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lysm.holamundolaura
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
